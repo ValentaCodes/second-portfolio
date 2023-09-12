2f9a05eacc6ad04f5779b380ca068e7d29236a96
ValentaCodes <valentacodes@gmail.com>
2023-09-12 09:46:31 -0600

    indentation

--- src/components/Resume/ResumeFile.jsx
+++ src/components/Resume/ResumeFile.jsx
@@ -1,20 +1,17 @@
 import { Icon, Stack } from '@mui/material';
 import { Download } from '@mui/icons-material';
-import resume from '../../assets/resume.pdf';
+import resume from '../../assets/Resume-for-website.pdf';
 import resumeScreenshot from '../../assets/resume-screenshot.png';
-import './Resume.css'
+import './Resume.css';
 const ResumeFile = () => {
   return (
-    <Stack sx={{justifyContent: 'center'}}>
+    <Stack sx={{ justifyContent: 'center' }}>
       <a href={resume} download='CorneliusDavis-Resume'>
         <Icon>
           <Download />
         </Icon>
       </a>
-          <img
-            src={resumeScreenshot}
-            alt="Cornelius Davis's Resume"
-          ></img>
+      <img src={resumeScreenshot} alt="Cornelius Davis's Resume"></img>
     </Stack>
   );
 };
