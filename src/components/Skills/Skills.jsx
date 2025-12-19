14ac1eca7aa262eca95efc412958d39acd11f3d9
Cornelius Davis <valentacodes@gmail.com>
2025-12-18 21:21:14 -0700

    fixed skill cutoff issues

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,88 +1,55 @@
-import { Box, Icon, Stack, Typography } from '@mui/material';
-import {
-  SiMongodb,
-  SiReact,
-  SiNodedotjs,
-  SiGraphql,
-  SiCss3,
-  SiHtml5,
-  SiExpress,
-  SiMysql,
-  SiGit,
-  SiMui,
-  SiRailway,
-  SiJavascript
-} from 'react-icons/si';
+import {Box, Icon, Stack, Typography} from '@mui/material';
+import {SiCss3, SiGit, SiHtml5, SiJavascript, SiMongodb, SiMui, SiMysql, SiReact} from 'react-icons/si';
 
-import {
-  FaJava
-} from "react-icons/fa";
+import {FaJava} from "react-icons/fa";
 import './Skills.css';
 
 import React from 'react';
 
 const Skills = () => {
-  return (
-    <>
-      <Stack className='icons'>
-        <Typography variant='h4'>My Skills</Typography>
-        <Box>
-          <Typography variant='h5'>Front-end Tech</Typography>
-          <ul>
-            <Icon className='icon'>
-              <SiReact />
-            </Icon>
-            <Icon className='icon'>
-              <SiJavascript />
-            </Icon>
-            <Icon className='icon'>
-              <SiCss3 />
-            </Icon>
-            <Icon className='icon'>
-              <SiHtml5 />
-            </Icon>
-          </ul>
-        </Box>
-        <Box paddingTop={"12px"}>
-          <Typography variant='h5'>Back-end Tech</Typography>
-          <ul>
-            <Icon className='icon'>
-              <FaJava />
-            </Icon>
-            <Icon className='icon'>
-              <SiNodedotjs />
-            </Icon>
-            <Icon className='icon'>
-              <SiMongodb />
-            </Icon>
-            <Icon className='icon'>
-              <SiMysql />
-            </Icon>
-            <Icon className='icon'>
-              <SiGraphql />
-            </Icon>
-            <Icon className='icon'>
-              <SiExpress />
-            </Icon>
-          </ul>
-        </Box>
-        <Box>
-          <Typography variant='h5'>Extra</Typography>
-          <ul>
-            <Icon className='icon'>
-              <SiGit />
-            </Icon>
-            <Icon className='icon'>
-              <SiMui />
-            </Icon>
-            <Icon className='icon'>
-              <SiRailway />
-            </Icon>
-          </ul>
-        </Box>
-      </Stack>
-    </>
-  );
+    return (
+        <>
+            <Stack className='icons'>
+                <Typography variant='h4'>My Skills</Typography>
+                <Box>
+                    <Typography variant='h5'>Programming Languages</Typography>
+                    <ul>
+                        <Icon className={'icon'}>
+                            <FaJava/>
+                        </Icon>
+                        <Icon className='icon'>
+                            <SiJavascript/>
+                        </Icon>
+                    </ul>
+                </Box>
+                <Box paddingTop={"6px"}>
+                    <Typography variant='h5'>Other</Typography>
+                    <ul>
+
+                        <Icon className='icon'>
+                            <SiReact/>
+                        </Icon>
+                        <Icon className='icon'>
+                            <SiCss3/>
+                        </Icon>
+                        <Icon className='icon'>
+                            <SiHtml5/>
+                        </Icon>
+                        <Icon className='icon'>
+                            <SiMongodb/>
+                        </Icon>
+                        <Icon className='icon'>
+                            <SiMysql/>
+                        </Icon>
+                        <Icon className='icon'>
+                            <SiGit/>
+                        </Icon>
+                    </ul>
+                </Box>
+
+            </Stack>
+        </>
+    );
 };
 
 export default Skills;
